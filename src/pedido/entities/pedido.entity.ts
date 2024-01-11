94b2fd92466dfcadd1fa2747b29729bb3cf8005e
Vinicius Costa <54358510+ViniciusCosta126@users.noreply.github.com>
2024-01-11 10:50:46 -0300

    Criando entidade pedidos e criando relacionamento com usuario

---
+++ src/pedido/entities/pedido.entity.ts
@@ -0,0 +1,35 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { StatusPedido } from '../enum/statuspedido.enum';
+import { UsuarioEntity } from '../../usuario/entities/usario.entity';
+
+@Entity({ name: 'pedidos' })
+export class PedidoEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ name: 'valor_total', nullable: false, type: 'real' })
+  valorTotal: number;
+
+  @Column({ name: 'status', nullable: false, enum: StatusPedido })
+  status: StatusPedido;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: string;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: string;
+
+  @DeleteDateColumn({ name: 'deleted_at' })
+  deletedAt: string;
+
+  @ManyToOne(() => UsuarioEntity, (usuario) => usuario.pedidos)
+  usuario: UsuarioEntity;
+}
