850bf233a89f75ba0685ed1b989e5af86b126162
Vinicius Costa <54358510+ViniciusCosta126@users.noreply.github.com>
2024-01-11 13:14:43 -0300

    Finalizando o relacionamento entre produto e pedido

--- src/pedido/pedido.module.ts
+++ src/pedido/pedido.module.ts
@@ -4,9 +4,12 @@ import { PedidoController } from './controllers/pedido.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PedidoEntity } from './entities/pedido.entity';
 import { UsuarioEntity } from '../usuario/entities/usario.entity';
+import { ProdutoEntity } from 'src/produtos/entities/Produto.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PedidoEntity, UsuarioEntity])],
+  imports: [
+    TypeOrmModule.forFeature([PedidoEntity, UsuarioEntity, ProdutoEntity]),
+  ],
   controllers: [PedidoController],
   providers: [PedidoService],
 })
--- src/pedido/services/pedido.service.ts
+++ src/pedido/services/pedido.service.ts
@@ -3,12 +3,12 @@ import { Injectable } from '@nestjs/common';
 import { UpdatePedidoDto } from '../dto/update-pedido.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { PedidoEntity } from '../entities/pedido.entity';
-import { Repository } from 'typeorm';
+import { FindRelationsNotFoundError, In, Repository } from 'typeorm';
 import { UsuarioEntity } from '../../usuario/entities/usario.entity';
 import { StatusPedido } from '../enum/statuspedido.enum';
 import { CriaPedidoDTO } from '../dto/CriaPedido.dto';
 import { ItemPedidoEntity } from '../entities/ItemPedido.entity';
-import { reduce } from 'rxjs';
+import { ProdutoEntity } from '../../produtos/entities/Produto.entity';
 
 @Injectable()
 export class PedidoService {
@@ -18,16 +18,29 @@ export class PedidoService {
 
     @InjectRepository(UsuarioEntity)
     private readonly usarioRepository: Repository<UsuarioEntity>,
+
+    @InjectRepository(ProdutoEntity)
+    private readonly produtoRepository: Repository<ProdutoEntity>,
   ) {}
 
   async cadastroPedido(usuarioId: string, dadosPedido: CriaPedidoDTO) {
     const usuario = await this.usarioRepository.findOneBy({ id: usuarioId });
+    const produtosIds = dadosPedido.itensPedido.map((item) => item.produtoId);
+    const produtosRelacionados = await this.produtoRepository.findBy({
+      id: In(produtosIds),
+    });
     const pedidoEntity = new PedidoEntity();
 
     const itensPedidosEntidades = dadosPedido.itensPedido.map((item) => {
       const itemPedidoEntity = new ItemPedidoEntity();
-      itemPedidoEntity.precoVenda = 10;
+      const produtoRelacionado = produtosRelacionados.find(
+        (produto) => produto.id === item.produtoId,
+      );
+
+      itemPedidoEntity.produto = produtoRelacionado;
+      itemPedidoEntity.precoVenda = produtoRelacionado.valor;
       itemPedidoEntity.quantidade = item.quantidade;
+      itemPedidoEntity.produto.quantidadeDisponivel -= item.quantidade;
       return itemPedidoEntity;
     });
 
